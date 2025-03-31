4d6643ae336702a731b02d520a2059c67bdd7914
san06032002 <sanskarpardesi@gmail.com>
2025-03-30 23:46:05 -0400

    removed the forcefull cors allowance

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// ✅ Manual CORS headers for Vercel + localhost
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); 
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-  next();
-});
+
 
 // Env variables
 const MONGO_URL = process.env.MONGO_URL;
